6beb2e1fb72a01621b617c157725adda76242150
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-03-25 23:59:32 +0300

    Example006 Bases

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,19 @@
+﻿//Максимальное число массива.
+int a = 3;
+int b = 5;
+int c = 4;
+int d = 8;
+int e = 2;
+int i = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (i > max) max = i;
+
+Console.Write("max = ");
+Console.WriteLine(max);
