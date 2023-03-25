9a381d5a64e98d57d60d2a75e4d67a9a4bf9ec62
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-03-25 23:22:10 +0300

    Example005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿//Приветствие пользователя.
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("Ура, да это же Маша красавица наша! ");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(username);
+}
