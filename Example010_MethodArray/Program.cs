4828b89e55198f9ac79ae212c4734f47609ea471
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-05 06:20:25 +0300

    Метод Массивов через индекса числа в массиве

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿//Массивы.
+int[] array = { 15, 56, 32, 4, 56, 46, 67, 56, 39 };
+
+int n = array.Length;
+int find = 56;
+
+int index = 0;
+
+while (index < n)
+{
+
+   if (array[index] == find)
+   {
+      Console.WriteLine(index);
+      break;
+   }
+   //index = index + 1;
+   index++;
+}
